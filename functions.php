9ad34f3cac25d1c0fcf2466dd68a912b236922eb
Lee Fuller <lee@weareempire.co.uk>
2015-09-08 16:35:04 +0100

    Commit
    Will now use this location in sublime

--- functions.php
+++ functions.php
@@ -1,10 +1,39 @@
 <?php
 
-  if ( file_exists( TEMPLATEPATH . '/update/update.php' ) ) :
 
-    require_once( TEMPLATEPATH . '/update/update.php' );
+/* ============================================================================
+  Include update directory to allow theme updates
+===============================================================================
+
+  Since:        0.0.001
+
+  Last Updated: 0.0.001
+
+============================================================================ */
+
+  if ( file_exists( TEMPLATEPATH . '/update/index.php' ) ) :
+
+    require_once( TEMPLATEPATH . '/update/index.php' );
 
   endif;
+
+
+/* ============================================================================
+  Include General WordPress functions, filters and actions
+===============================================================================
+
+  Since:        0.0.001
+
+  Last Updated: 0.0.001
+
+  - Generic functions including:
+
+    - acf_add_options_page
+    - emp_getMobileProperties
+    - emp_getSocialProperties
+    - custom_walker
+
+============================================================================ */
 
   if ( file_exists( get_template_directory() . '/functions/general/functions.php' ) ) :
 
@@ -13,6 +42,50 @@
   endif;
 
 
+/* ============================================================================
+  Include Optimisation functions, filters and actions
+===============================================================================
+
+  Since:        0.0.001
+
+  Last Updated: 0.0.001
+
+  - Optimisation functions including:
+
+    - clean_wp_header
+    - Soil
+    - Sanitize
+
+============================================================================ */
+
+  if ( file_exists( get_template_directory() . '/functions/woocommerce/functions.php' ) ) :
+
+    include_once( get_template_directory() . '/functions/woocommerce/functions.php' );
+
+  endif;
+
+
+/* ============================================================================
+  Include WooCommerce functions, filters and actions
+===============================================================================
+
+  Since:        0.0.001
+
+  Last Updated: 0.0.001
+
+  - WooCommerce functions including:
+
+    - 
+
+  - Checks if the plugin is activated before including
+
+============================================================================ */
+
+  if ( is_plugin_active( 'woocommerce/woocommerce.php' ) && file_exists( get_template_directory() . '/functions/woocommerce/functions.php' ) ) :
+
+    include_once( get_template_directory() . '/functions/woocommerce/functions.php' );
+
+  endif;
 
 
 
